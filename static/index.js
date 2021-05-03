ee4be47aca4bab7b3f493e99c595567d75389996
danharris-stu <daniel.harris@keyin.com>
2021-05-03 12:05:49 -0230

    mvp

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 function main(){
+    // Placeholder for more complex data loading in the future
     console.log("Main...")
 }
-
 
 //Run main on HTML load completion
 window.addEventListener('load', main);
